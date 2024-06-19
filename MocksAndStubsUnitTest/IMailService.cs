355a66c8421a448eb51614caa9ec6e4b69bb005b
mihee <mihee@MILHOT>
2024-06-19 23:45:45 +0300

    Добавьте файлы проекта.

---
+++ MocksAndStubsUnitTest/IMailService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MocksAndStubsUnitTest
+{
+    public interface IMailService
+    {
+        void SendMail(string destination, string message);
+    }
+}
---
+++ MocksAndStubsUnitTest/MockLogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MocksAndStubsUnitTest
+{
+    class MockLogService : ILogService
+    {
+        public string? lastError;
+
+        public void LogError(string error)
+        {
+            lastError = error;
+        }
+    }
+}
